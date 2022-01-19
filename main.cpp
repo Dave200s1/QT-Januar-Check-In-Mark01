46a9a3d8dce44902c54f10929da8d695538e73a8
Dave200s1 <81190030+Dave200s1@users.noreply.github.com>
2022-01-19 09:53:47 +0100

    Add files via upload
    QFile Read n Write

--- main.cpp
+++ main.cpp
@@ -1,23 +1,39 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
-#include<iostream>
-#include<fstream>
-#include<string>
 
-using namespace std;
+void basics()
+{
 
+    QFile File("text.txt");
+    if(!File.open(QIODevice::OpenModeFlag::Append))
+    {
+        qCritical()<<"Could not open files!";
+        qCritical()<<File.errorString();
+        return;
+
+    }
+
+    qInfo()<<"Writing file..";
+    File.write(QByteArray("Hello World"));
+    File.flush();//pushes the data to the device
+
+    qInfo()<<"Reading file..";
+    File.seek(0);//0 beginnig of a file
+    qInfo()<<File.readAll();
+
+    File.close();
+}
 
 int main(int argc, char *argv[])
 {
-
-
     QApplication a(argc, argv);
+    basics();
     MainWindow w;
-    w.showFullScreen();
-    //w.show();
+    w.show();
     return a.exec();
-
-
 }
